46e9042b18675f036fde9ff9d27600bc596bca5e
Yilin Chen <sticnarf@gmail.com>
2018-10-29 21:54:45 +0800

    Parse chnroutes into IpRange

--- src/resolver/mix.rs
+++ src/resolver/mix.rs
@@ -1,6 +1,14 @@
 use super::*;
 use crate::LOGGER;
 
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+use std::str::FromStr;
+
+use failure::Error;
+use ipnet::Ipv4Net;
+use iprange::{IpNet, IpRange};
 use tokio::prelude::*;
 use trust_dns_proto::error::ProtoError;
 use trust_dns_proto::xfer::DnsResponse;
@@ -13,6 +21,7 @@ where
 {
     china: C,
     foreign: F,
+    china_ip: IpRange<Ipv4Net>,
 }
 
 impl<C, F> MixedResolver<C, F>
@@ -20,8 +29,27 @@ where
     C: Resolver,
     F: Resolver,
 {
-    pub fn new(china: C, foreign: F) -> Self {
-        MixedResolver { china, foreign }
+    fn parse_chnroutes<P, N>(path: P) -> Result<IpRange<N>, Error>
+    where
+        P: AsRef<Path>,
+        N: IpNet + FromStr,
+    {
+        let f = File::open(path)?;
+        let reader = BufReader::new(f);
+        Ok(reader
+            .lines()
+            .flat_map(|l| l)
+            .filter(|line| !line.starts_with("#"))
+            .flat_map(|l| l.parse::<N>())
+            .collect())
+    }
+
+    pub fn new<P: AsRef<Path>>(china: C, foreign: F, china_ip_path: P) -> Result<Self, Error> {
+        Ok(MixedResolver {
+            china,
+            foreign,
+            china_ip: Self::parse_chnroutes(china_ip_path)?,
+        })
     }
 }
 
