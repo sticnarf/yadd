440fa1b97639995002d1fcd67f1d0c9a7e1b2829
Yilin Chen <sticnarf@gmail.com>
2018-11-04 12:40:32 +0800

    Use japaric/trust for CI (#4)
    * Use japaric/trust
    
    * Do not run the program on tests
    
    * Remove android and ios targets
    
    * Do not depend on ring
    
    * Use latest cross instead
    
    * Build docker container
    
    * Force overwrite cross
    
    * Fix for aarch64
    
    * Add appveyor config
    
    * Do not run the program on Windows

---
+++ ci/install.sh
@@ -0,0 +1,70 @@
+set -ex
+
+main() {
+    # local target=
+    # if [ $TRAVIS_OS_NAME = linux ]; then
+    #     target=x86_64-unknown-linux-musl
+    #     sort=sort
+    # else
+    #     target=x86_64-apple-darwin
+    #     sort=gsort  # for `sort --sort-version`, from brew's coreutils.
+    # fi
+
+    # Builds for iOS are done on OSX, but require the specific target to be
+    # installed.
+    case $TARGET in
+        aarch64-apple-ios)
+            rustup target install aarch64-apple-ios
+            ;;
+        armv7-apple-ios)
+            rustup target install armv7-apple-ios
+            ;;
+        armv7s-apple-ios)
+            rustup target install armv7s-apple-ios
+            ;;
+        i386-apple-ios)
+            rustup target install i386-apple-ios
+            ;;
+        x86_64-apple-ios)
+            rustup target install x86_64-apple-ios
+            ;;
+    esac
+
+    # This fetches latest stable release
+    # local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+    #                    | cut -d/ -f3 \
+    #                    | grep -E '^v[0.1.0-9.]+$' \
+    #                    | $sort --version-sort \
+    #                    | tail -n1)
+    # curl -LSfs https://japaric.github.io/trust/install.sh | \
+    #     sh -s -- \
+    #        --force \
+    #        --git japaric/cross \
+    #        --tag $tag \
+    #        --target $target
+
+    local src=$(pwd) \
+          tmp=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            tmp=$(mktemp -d)
+            ;;
+        osx)
+            tmp=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    cargo install --git https://github.com/rust-embedded/cross --force
+
+    if [ $TRAVIS_OS_NAME = linux ]; then
+        cd $tmp
+        git clone https://github.com/rust-embedded/cross.git
+        cd cross
+        sh build-docker-image.sh $TARGET
+    fi
+    
+    cd $src
+}
+
+main
